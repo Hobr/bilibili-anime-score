0d920c3b703e22006b791100018938675f3c5557
Hobr <mkowes@vip.qq.com>
2019-07-29 10:19:03 +0800

    api update

--- get.py
+++ get.py
@@ -13,11 +13,8 @@ def bilibili_rating(bangumi_id):
     response = requests.get(url.format(bangumi_id), params=payload)
     data = json.loads(response.text[19:-2])
     try:
-
         area = "\"{0}\"".format(data["result"]["area"]) # 地区
-        cover = "\"{0}\"".format(data["result"]["cover"]) # logo
         danmaku_count = int(data["result"]["danmaku_count"]) # 弹幕
-        evaluate = "\"{0}\"".format(data["result"]["evaluate"][0:60].replace('\"','')) # 介绍
         favorites = int(data["result"]["favorites"]) # 追番
         is_finish = int(data["result"]["is_finish"]) # 完结
         count = float(data["result"]["media"]["rating"]["score"]) # 人数
@@ -25,12 +22,10 @@ def bilibili_rating(bangumi_id):
         title = "\"{0}\"".format(data["result"]["media"]["title"]) # 名称
         play_count = int(data["result"]["play_count"]) # 播放量
         season_id = int(data["result"]["season_id"]) # seasonid
-        share_url = "\"{0}\"".format(data["result"]["share_url"]) # 链接
-        
         print(season_id, title, score, count,area)
         try:
-            cursor.execute("insert into bangumi_20171107 values ({0}, {1}, {2}, {3}, {4},{5},{6},{7},{8},{9},{10},{11})"
-                           .format(season_id, title, score, count, is_finish,favorites,area,cover,danmaku_count,evaluate,play_count,share_url))
+            cursor.execute("insert into bangumi values ({0}, {1}, {2}, {3}, {4},{5},{6},{7},{8})"
+                           .format(season_id, title, score, count, is_finish,favorites,area,danmaku_count,play_count))
             conn.commit()
         except sqlite3.IntegrityError:
             pass
@@ -41,7 +36,7 @@ if os.path.getsize("bilibili_bangumi.db") >= 2: # 数据库已创建
     conn = sqlite3.connect("bilibili_bangumi.db")
     cursor = conn.cursor()
     session = requests.session()
-    cursor.execute("select MAX(season_id) from bangumi_20171107")
+    cursor.execute("select MAX(season_id) from bangumi")
     lid = cursor.fetchone()
     
     try:
@@ -69,9 +64,9 @@ if os.path.getsize("bilibili_bangumi.db") >= 2: # 数据库已创建
 else: # 数据库未创建
     conn = sqlite3.connect("bilibili_bangumi.db")
     cursor = conn.cursor()
-    cursor.execute("create table bangumi_20171107 (season_id int primary key, "
-                "title varchar(255), score float, count int, is_finish int, favorites int,area varchar(40), cover varchar(255), danmaku_count int, evaluate varchar(25), playcount int, url varchar(255))")
-                #         名称         分数           人数        完结            追番         国家地区            LOGO               弹幕                   介绍               播放量       URL
+    cursor.execute("create table bangumi (season_id int primary key, "
+                "title varchar(255), score float, count int, is_finish int, favorites int,area varchar(40), danmaku_count int, playcount int")
+                #         名称         分数           人数        完结            追番         国家地区              弹幕                播放量
     session = requests.session()
     for i in range(0, 7000):
         bilibili_rating(i)
