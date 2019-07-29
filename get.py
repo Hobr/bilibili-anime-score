58742d25446eb2316b13cfcceb768f079cb48d42
Hobr <mkowes@vip.qq.com>
2019-07-29 09:36:42 +0800

    Support Python3.4

--- get.py
+++ get.py
@@ -26,7 +26,7 @@ def bilibili_rating(bangumi_id):
         play_count = int(data["result"]["play_count"]) # 播放量
         season_id = int(data["result"]["season_id"]) # seasonid
         share_url = "\"{0}\"".format(data["result"]["share_url"]) # 链接
-
+        
         print(season_id, title, score, count,area)
         try:
             cursor.execute("insert into bangumi_20171107 values ({0}, {1}, {2}, {3}, {4},{5},{6},{7},{8},{9},{10},{11})"
@@ -43,18 +43,18 @@ if os.path.getsize("bilibili_bangumi.db") >= 2: # 数据库已创建
     session = requests.session()
     cursor.execute("select MAX(season_id) from bangumi_20171107")
     lid = cursor.fetchone()
-    lastid = int(lid)
-    print(lastid[0])
-    if lastid[0] < 7000 :
-        for i in range(lastid[0]+1, 7000):
+    lastid = int(lid[0])
+    print(lastid)
+    if lastid < 7000 :
+        for i in range(lastid+1, 7000):
             bilibili_rating(i)
             print(i)
         for i in range(20000, 23000):
             bilibili_rating(i)
             print(i)
 
-    elif lastid[0] > 7000 and lastid[0] < 23000:
-        for i in range(lastid[0]+1, 23000):
+    elif lastid > 7000 and lastid < 23000:
+        for i in range(lastid+1, 23000):
             bilibili_rating(i)
             print(i)
 
