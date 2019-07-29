6b0aaab26a7c8bc1927fe0818396052cd09abaf2
Hobr <mkowes@vip.qq.com>
2019-07-29 09:30:08 +0800

    Update get.py

--- get.py
+++ get.py
@@ -42,7 +42,7 @@ if os.path.getsize("bilibili_bangumi.db") >= 2: # 数据库已创建
     cursor = conn.cursor()
     session = requests.session()
     cursor.execute("select MAX(season_id) from bangumi_20171107")
-    lastid = cursor.fetchone()
+    lastid = int(cursor.fetchone())
     print(lastid[0])
     if lastid[0] < 7000 :
         for i in range(lastid[0]+1, 7000):
