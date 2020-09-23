7ce4402096ade63424b30322615d38a0fe1ea87f
Hobr <mkowes@vip.qq.com>
2020-09-23 20:51:32 +0800

    little edit

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# Bilibili Bangumi
+# Bilibili Bangumi Get
 
-利用 bilibili 前端用到的 json 接口获取 Bilibili 动画评分并保存到数据库
+利用 bilibili 前端用到的 json 接口获取 Bilibili 动画相关数据并保存到数据库
 
 ## 使用
 
 ```bash
-> git clone https://github.com/Hobr/bilibili-anime-score.git
+> gh repo clone Hobr/bilibili-anime-score
 
 > pip install requests
 
@@ -14,4 +14,4 @@
 > python3 get.py
 ```
 
-结束后将提示 Done well 完整数据将保存于 bilibili_bangumi.db
+完整数据将保存于 bilibili_bangumi.db
--- get.py
+++ get.py
@@ -6,27 +6,27 @@ import json
 import sqlite3
 import os
 import time
-
 apiurl = "https://www.biliplus.com/api/bangumi?season={0}"
 
 
 def bilibili_rating(bangumi_id):
     time.sleep(1)
+    print(bangumi_id)
     response = requests.get(apiurl.format(bangumi_id))
     data = json.loads(response.text)
     if int(data["code"]) == 10:
         pass
     else:
         try:
-            area = "\"{0}\"".format(data["result"]["area"])  # 地区
-            danmaku_count = int(data["result"]["danmaku_count"])  # 弹幕
-            favorites = int(data["result"]["favorites"])  # 追番
-            is_finish = int(data["result"]["is_finish"])  # 完结
-            count = int(data["result"]["media"]["rating"]["count"])  # 评分人数
-            score = float(data["result"]["media"]["rating"]["score"])  # 评分
+            area = "\"{0}\"".format(data["result"]["area"])             # 地区
+            danmaku_count = int(data["result"]["danmaku_count"])        # 弹幕
+            favorites = int(data["result"]["favorites"])                # 追番
+            is_finish = int(data["result"]["is_finish"])                # 完结
+            count = int(data["result"]["media"]["rating"]["count"])     # 评分人数
+            score = float(data["result"]["media"]["rating"]["score"])   # 评分
             title = "\"{0}\"".format(data["result"]["media"]["title"])  # 名称
-            play_count = int(data["result"]["play_count"])  # 播放量
-            season_id = int(data["result"]["season_id"])  # seasonid
+            play_count = int(data["result"]["play_count"])              # 播放量
+            season_id = int(data["result"]["season_id"])                # id
             try:
                 cursor.execute("insert into bangumi values ({0}, {1}, {2}, {3}, {4},{5},{6},{7},{8})"
                                .format(season_id, title, score, count, is_finish, favorites, area, danmaku_count, play_count))
@@ -37,8 +37,10 @@ def bilibili_rating(bangumi_id):
             return None
 
 
-if os.path.getsize("bilibili_bangumi.db") >= 2:  # 数据库已创建
+# 数据库已创建
+if os.path.getsize("bilibili_bangumi.db") >= 2:
     conn = sqlite3.connect("bilibili_bangumi.db")
+    print("数据库已连接！")
     cursor = conn.cursor()
     session = requests.session()
     cursor.execute("select MAX(season_id) from bangumi")
@@ -51,19 +53,17 @@ if os.path.getsize("bilibili_bangumi.db") >= 2:  # 数据库已创建
     if lastid < 7000:
         for i in range(lastid+1, 7000):
             bilibili_rating(i)
-            print(i)
         for i in range(20000, 23000):
             bilibili_rating(i)
-            print(i)
     elif lastid > 7000 and lastid < 23000:
         for i in range(lastid+1, 23000):
             bilibili_rating(i)
-            print(i)
-    print("Done well")
+    print("任务完成")
     cursor.close()
     conn.close()
-
-else:  # 数据库未创建
+# 数据库未创建
+else:
+    print("数据库不存在，已自动创建！")
     conn = sqlite3.connect("bilibili_bangumi.db")
     cursor = conn.cursor()
     cursor.execute("create table bangumi (season_id int primary key, "
@@ -72,7 +72,6 @@ else:  # 数据库未创建
     session = requests.session()
     for i in range(0, 30000):
         bilibili_rating(i)
-        print(i)
-    print("Done well")
+    print("任务完成")
     cursor.close()
     conn.close()
